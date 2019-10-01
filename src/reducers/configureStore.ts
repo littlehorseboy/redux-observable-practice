7bdd3841b078d308108a5ae8ff3e70d303d126f6
horse <littlehorseboy@gmail.com>
2019-10-01 12:14:23 +0800

    feat: 定義 fetchUserEpic，FetchGithubUser.tsx 還沒改

--- src/reducers/configureStore.ts
+++ src/reducers/configureStore.ts
@@ -2,12 +2,18 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import pingingReducer from './pinging/pinging';
 import { pingingEpic } from '../actions/pinging/pinging';
+import fetchGithubUserReducer from './fetchGithubUser/fetchGithubUser';
+import { fetchUserEpic } from '../actions/fetchGithubUser/fetchGithubUser';
 
 const rootReducer = combineReducers({
   pingingReducer,
+  fetchGithubUserReducer,
 });
 
-const rootEpic = combineEpics(pingingEpic);
+const rootEpic = combineEpics(
+  pingingEpic,
+  fetchUserEpic,
+);
 
 const epicMiddleware = createEpicMiddleware();
 
